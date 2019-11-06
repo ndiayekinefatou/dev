7a8572a34a71d678ac788671b11ad0bd935a8709
ndiayekinefatou <kinendiaye4991@gmail.com>
2019-11-06 15:28:19 +0000

    first commit

---
+++ src/main/java/genieLogiciel/HelloWorld.java
@@ -0,0 +1,20 @@
+package genieLogiciel;
+
+public class HelloWorld {
+
+	
+	public String getMessage(String a)
+	{
+		
+		if(a==null)
+		{
+			return " ";
+		}
+		else return a;
+	}
+	
+	public int sum(int a,int b)
+	{
+		return a+b;
+	}
+}
